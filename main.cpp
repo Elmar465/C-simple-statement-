16ea898cd61012df6c6dcafcbc28935c67f3edf8
Elmar465 <Elmar465@gmail.com>
2023-03-06 18:22:04 +0400

    C++ simple statements

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include  <iostream>
+
+int main() {
+    std::cout << "Number 1" << std::endl; 
+    std::cout << "Number 2" << std::endl;
+    std::cout << "Number 3" << std::endl;
+    std::cout << "Elmar Ceferli" << std::endl;
+
+
+    //  In this lesson we learned simple print statements and what is the compile meaning and type of compilers and also we learned error types while building task in C++ 
+}
